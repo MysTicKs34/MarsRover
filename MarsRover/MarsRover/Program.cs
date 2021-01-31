6e48adc63139c7e3cf152748c6b2205a90af2a64
ersintunbul <ersintunbul@hotmail.com>
2021-01-31 18:35:02 +0300

    console app added

---
+++ MarsRover/MarsRover/Program.cs
@@ -0,0 +1,38 @@
+﻿using MarsRover.Core.Abstraction;
+using MarsRover.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MarsRover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+            .AddSingleton<IPlateauService, PlateauService>()
+            .AddSingleton<ICalculateService, CalculateService>()
+            .AddSingleton<IRoverService, RoverService>()
+            .AddSingleton<IOperationService, OperationService>()
+            .AddLogging()
+            .BuildServiceProvider();
+
+            serviceProvider.GetService<IOperationService>().CreateConsoleScreen();
+
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to exit...");
+
+            Exit();
+        }
+
+        public static void Exit()
+        {
+            var c = Console.ReadKey();
+            if (c.Key == ConsoleKey.Enter)
+            {
+                return;
+            }
+            Exit();
+        }
+    }
+}
