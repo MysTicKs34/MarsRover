143414f346e41d48e7c770c6ae7e48d14dc444b8
ersintunbul <ersintunbul@hotmail.com>
2021-01-31 17:51:52 +0300

    IRoverService.cs for marsrover.core added

---
+++ MarsRover/MarsRover.Core/Abstraction/IRoverService.cs
@@ -0,0 +1,9 @@
+﻿using MarsRover.Model;
+
+namespace MarsRover.Core.Abstraction
+{
+    public interface IRoverService
+    {
+        Rover CreateRover(string positionCommand, string instructionCommand);
+    }
+}
