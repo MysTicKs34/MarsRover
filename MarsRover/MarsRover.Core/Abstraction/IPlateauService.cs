fced1c4ada2d66728ec8fa67595c080596001de3
ersintunbul <ersintunbul@hotmail.com>
2021-01-31 17:51:33 +0300

    IPlateauService.cs for marsrover.core added

---
+++ MarsRover/MarsRover.Core/Abstraction/IPlateauService.cs
@@ -0,0 +1,9 @@
+﻿using MarsRover.Model;
+
+namespace MarsRover.Core.Abstraction
+{
+    public interface IPlateauService
+    {
+        Plateau CreatePlateau(string upperRightCoordinate);
+    }
+}
