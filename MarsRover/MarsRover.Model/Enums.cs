7389478684f18b059978dbe41e42831c3dd51fea
ersintunbul <ersintunbul@hotmail.com>
2021-01-31 18:03:52 +0300

    Enums.cs for marsrover.model added

---
+++ MarsRover/MarsRover.Model/Enums.cs
@@ -0,0 +1,18 @@
+﻿namespace MarsRover.Model
+{
+    public enum Orientation
+    {
+        Undefined,
+        N,
+        E,
+        S,
+        W
+    }
+
+    public enum Direction
+    {
+        Left = -1,
+        Undefined = 0,
+        Right = 1
+    }
+}
