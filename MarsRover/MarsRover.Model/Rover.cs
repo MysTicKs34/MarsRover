9f114312740d2d4d84d8424a654997603a52d559
ersintunbul <ersintunbul@hotmail.com>
2021-01-31 18:04:33 +0300

    Rover.cs for marsrover.model added

---
+++ MarsRover/MarsRover.Model/Rover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MarsRover.Model
+{
+    public class Rover
+    {
+
+        public Rover()
+        {
+            StartPosition = new RoverPosition();
+            PositionHistory = new List<RoverPosition>();
+        }
+        public RoverPosition StartPosition { get; set; }
+        public RoverPosition LastPosition { get; set; }
+        public List<RoverPosition> PositionHistory { get; set; }
+        public string InstructionCommand { get; set; }
+
+    }
+}
